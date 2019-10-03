6c8966350986e67a71c3a70e0b1b3cb2f452e57c
fentonhaslam <fentonhaslam@gmail.com>
2019-10-03 12:38:15 +0100

    created getBass/mid/trebleEnergy methods

--- app/javascript/audioFunctions/audioTool.js
+++ app/javascript/audioFunctions/audioTool.js
@@ -3,23 +3,24 @@ class AudioTool {
   static async getAudioInput(audioCtx) {
 
     if (navigator.mediaDevices.getUserMedia) {
-        console.log('getUserMedia supported');
-        var constraints = {audio: true}
-        try {
+      console.log('getUserMedia supported');
+      var constraints = {
+        audio: true
+      }
+      try {
         const stream = await navigator.mediaDevices.getUserMedia(constraints)
         return audioCtx.createMediaStreamSource(stream);
-        }
-        catch (err) {
-          console.log('This following gUM error occured: ' + err);
-        }
+      } catch (err) {
+        console.log('This following gUM error occured: ' + err);
+      }
     } else {
-        console.log('getUserMedia not supported on your browser!');
+      console.log('getUserMedia not supported on your browser!');
     }
 
   }
 
   static getAnalyser(audioStream, audioContext) {
-    
+
     const audioAnalyserNode = audioContext.createAnalyser();
     audioStream.connect(audioAnalyserNode);
 
@@ -32,6 +33,18 @@ class AudioTool {
     let dataArray = new Uint8Array(bufferLength);
     analyser.getByteFrequencyData(dataArray);
     return dataArray
+  }
+
+  static getBassEnergy() {
+    return this.getLevels(analyser).slice(0, 24);
+  }
+
+  static getMidEnergy() {
+    return this.getLevels(analyser).slice(25, 204);
+  }
+
+  static getTrebleEnergy() {
+    return this.getLevels(analyser).slice(205, 1023);
   }
 }
 

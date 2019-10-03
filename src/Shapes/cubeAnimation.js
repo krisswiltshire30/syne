5eeabf05e78ebb0a5716f0f12c369f72b45aff1c
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-10-03 17:41:04 +0100

    Removed canvas and camera creation from cubeAnimation
    Co-authored-by: GuyCalloway <guyfcalloway@gmail.com>

--- src/Shapes/cubeAnimation.js
+++ src/Shapes/cubeAnimation.js
@@ -1,30 +1,19 @@
 class Cube {
   constructor() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
     this.geometry = new THREE.BoxGeometry(1, 1, 1);
     this.material = new THREE.MeshBasicMaterial({
       color: 0x00ff00
     });
     this.cube = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.cube);
-    this.camera.position.z = 4;
+    scene.add(this.cube);
   }
 
-  animate(size) {
+  animate(size, position) {
     var animateShape = function (instance) {
       instance.cube.scale.set(size, size, size);
-      requestAnimationFrame(animateShape(this));
-      instance.renderer.render(instance.scene, instance.camera);
-    }
+      renderer.render(scene, camera);
+    };
+    this.cube.position.y = (position, position, position);
     animateShape(this);
   }
 }

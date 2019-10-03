2c8fb9810f238c99ab4f60279ffa7dee4b979632
Guy Calloway <[guyfcalloway@gmail.com]>
2019-10-03 18:59:43 +0100

    Feature: Sphere Animation
    Co-authored-by: krisswiltshire30 <kriss.wiltshire@googlemail.com>

---
+++ src/Shapes/sphereAnimation.js
@@ -0,0 +1,21 @@
+
+class Sphere {
+    constructor() {
+       this.geometry = new THREE.SphereGeometry(5);
+       this.material = new THREE.MeshBasicMaterial( {color: 0xffff00}  );
+       this.sphere = new THREE.Mesh(this.geometry, this.material );
+       scene.add( this.sphere );
+    }
+
+    animate(size, position) {
+        var animateShape = function(instance) {
+          instance.sphere.scale.x = size;
+          instance.sphere.scale.y = size;
+          instance.sphere.scale.z = size;
+          renderer.render(scene, camera);
+      }
+      this.sphere.position.y = (position, position, position);
+      animateShape(this);
+    }
+
+};

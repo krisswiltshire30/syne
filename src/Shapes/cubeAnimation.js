d6849c980f0ac9c9c803628eec3da0bd61ddba05
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-10-03 15:55:42 +0100

    Setup for testing

---
+++ src/Shapes/cubeAnimation.js
@@ -0,0 +1,29 @@
+class Cube {
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    this.geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00
+    });
+    this.cube = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.cube);
+    this.camera.position.z = 4;
+  }
+
+  animate(size) {
+    var animateShape = function (instance) {
+      instance.cube.scale.set(size, size, size);
+      instance.renderer.render(instance.scene, instance.camera);
+    }
+    animateShape(this);
+  }
+}

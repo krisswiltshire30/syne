9d82d3fd3c06c044f876284ef2d97bd60adb6bd0
fentonhaslam <fentonhaslam@gmail.com>
2019-10-03 14:35:59 +0100

    merging all locally

--- public/index.js
+++
@@ -1,8 +0,0 @@
-async function init() {
-  let audioContext = new AudioContext;
-  let audioIn = await AudioTool.getAudioInput(audioContext);
-  let analyser = AudioTool.getAnalyser(audioIn, audioContext);
-  window.analyser = analyser;
-}
-
-init()

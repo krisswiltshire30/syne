06a9564d16153cc36807a9d216186b7a0f58a869
fentonhaslam <fentonhaslam@gmail.com>
2019-10-03 16:50:29 +0100

    created get analyser node test and passed.
    Co-authored-by: RMaughan93 <rhys.maughan93@gmail.com>

--- spec/AudioTool/audioToolSpec.js
+++ spec/AudioTool/audioToolSpec.js
@@ -1,12 +1,22 @@
 describe('AudioTool', () => {
-    beforeEach(()=> {
-        var audioCtx = new AudioContext;
+
+    var audioCtx = new AudioContext;
+    var stream = AudioTool.getAudioInput(audioCtx);
+
+    beforeEach(() => {
+        spyOn(AudioTool, 'getAnalyser').and.returnValue(AnalyserNode);
     });
 
-    it('Should be async and return media stream', function(done) {
-        AudioTool.getAudioInput(audioCtx).then(function(result) {
+    it('Should be async and return media stream', function (done) {
+        AudioTool.getAudioInput(audioCtx).then(function (result) {
             expect(result.constructor.name).toBe('MediaStreamAudioSourceNode');
             done();
         });
     });
+
+    describe('::getAnalyser', () => {
+        it('returns an audio analyser node', function () {
+            expect(AudioTool.getAnalyser(stream, audioCtx)).toEqual(AnalyserNode);
+        })
+    })
 });

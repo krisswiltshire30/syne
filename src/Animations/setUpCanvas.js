226fe481255019124a1019ce8c60f8fb55dc9f57
RMaughan93 <rhys.maughan93@gmail.com>
2019-10-04 16:33:47 +0100

    trying to make travis happy

--- src/Animations/setUpCanvas.js
+++ src/Animations/setUpCanvas.js
@@ -1,12 +1,12 @@
-var THREE = require('three');
+import { WebGLRenderer, Scene, PerspectiveCamera, HemisphereLight } from 'three';
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+var scene = new Scene();
+var camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.y = -800;
 camera.position.z = 800;
 camera.rotation.x = 0.70;
-var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+var light = new HemisphereLight( 0xffffbb, 0x080820, 1 );
 scene.add( light );
--- src/Animations/shapesAnimation.js
+++ src/Animations/shapesAnimation.js
@@ -1,5 +1,3 @@
-var THREE = require('three');
-
 const sphere1 = new Sphere(150, -500, 0, 0, scene);
 const cube1 = new Cube(200, 200, 200, 0, 0, 0, scene);
 const tetra1 = new Tetra(200, 500, 0, 0, scene);

141ce8cb2d7c092d6ef5220516a8b70e9b93ae3c
Guillaume <liard.guillaume@gmail.com>
2019-10-04 10:18:03 +0100

    Fixed tests for getAudioInput and getAnalyser
    Co-authored-by: Fentonhaslam <fentonhaslam@gmail.com>
    Co-authored-by: RMaughan93 <rhys.maughan93@gmail.com>

--- spec/AudioTool/audioToolSpec.js
+++ spec/AudioTool/audioToolSpec.js
@@ -1,22 +1,30 @@
 describe('AudioTool', () => {
 
     var audioCtx = new AudioContext;
-    var stream = AudioTool.getAudioInput(audioCtx);
 
-    beforeEach(() => {
-        spyOn(AudioTool, 'getAnalyser').and.returnValue(AnalyserNode);
-    });
+    describe('getAudioInput', () => {
 
-    it('Should be async and return media stream', function (done) {
+      it('Should be async and return media stream', function (done) {
         AudioTool.getAudioInput(audioCtx).then(function (result) {
             expect(result.constructor.name).toBe('MediaStreamAudioSourceNode');
             done();
         });
+      });
+
     });
+    
 
     describe('::getAnalyser', () => {
-        it('returns an audio analyser node', function () {
-            expect(AudioTool.getAnalyser(stream, audioCtx)).toEqual(AnalyserNode);
-        })
-    })
+      
+      it('returns an audio analyser node', function () {
+        let stream = jasmine.createSpyObj('AudioNode', {
+          'connect': true
+        });
+        let analyser = AudioTool.getAnalyser(stream, audioCtx);
+
+        expect(analyser.constructor.name).toEqual('AnalyserNode');
+      });
+
+    });
+    
 });

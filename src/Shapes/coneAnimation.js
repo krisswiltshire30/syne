a0444f68205ee17331b34cc3dfe992cf6e330354
Guy Calloway <[guyfcalloway@gmail.com]>
2019-10-04 11:20:56 +0100

    Feature: Cylinder - simplified

--- src/Shapes/coneAnimation.js
+++ src/Shapes/coneAnimation.js
@@ -1,34 +1,20 @@
 class Cone {
     constructor() {
+        //do we want to input geometry values into constructor?
         this.geometry = new THREE.ConeGeometry( 5, 20, 32 );
         this.material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
         this.cone = new THREE.Mesh( this.geometry, this.material );
         scene.add( this.cone );
+        renderer.render(scene, camera);
     }
 
-    animate(sizeScalerX, sizeScalerY, sizeScalerZ, position) {
+    animate(sizeScalerX, sizeScalerY, sizeScalerZ) {
         var animateShape = function(instance) {
           instance.cone.scale.x = sizeScalerX;
           instance.cone.scale.y = sizeScalerY;
           instance.cone.scale.z = sizeScalerZ;
           renderer.render(scene, camera);
       }
-      this.cone.position.x = position;
       animateShape(this);
     }
 };
-
-
-// changing origin of rotation
-// var cyl_material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-// var cyl_width = 1;
-// var cyl_height = 5;
-// // THREE.CylinderGeometry(bottomRadius, topRadius, height, segmentsRadius, 
-//  segmentsHeight, openEnded )
-//  var cylGeometry = new THREE.CylinderGeometry(cyl_width, cyl_width, cyl_height, 20, 1, false);
-// // translate the cylinder geometry so that the desired point within the 
-// geometry is now at the origin
-// cylGeometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, cyl_height/2, 0 ) );
-// var cylinder = new THREE.Mesh(cylGeometry, cyl_material);
-
-// scene.add( cylinder );

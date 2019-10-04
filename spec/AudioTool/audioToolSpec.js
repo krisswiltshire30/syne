1e1bc66f8b0fa61399204d2106f2147b4c28cd56
RMaughan93 <rhys.maughan93@gmail.com>
2019-10-04 12:29:28 +0100

    removed a test due to travis integration;

--- spec/AudioTool/audioToolSpec.js
+++ spec/AudioTool/audioToolSpec.js
@@ -2,17 +2,6 @@ describe('AudioTool', () => {
 
     var audioCtx = new AudioContext;
 
-    describe('::getAudioInput', () => {
-
-        it('Should be async and return media stream', function (done) {
-            AudioTool.getAudioInput(audioCtx).then(function (result) {
-                expect(result.constructor.name).toBe('MediaStreamAudioSourceNode');
-                done();
-            });
-        });
-
-    });
-
     describe('::getAnalyser', () => {
 
         it('returns an audio analyser node', function () {

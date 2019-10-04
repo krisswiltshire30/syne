b578bd8eccee36e42ae7e020bbc0cecdacc5effa
fentonhaslam <fentonhaslam@gmail.com>
2019-10-04 11:01:59 +0100

    checked if length is correct from analyser array.
    let stream = jasmine.createSpyObj('AudioNode', {
              'connect': true
            });
            let analyser = AudioTool.getAnalyser(stream, audioCtx);
    
    Co-authored-by: RMaughan93 <rhys.maughan93@gmail.com>

--- spec/AudioTool/audioToolSpec.js
+++ spec/AudioTool/audioToolSpec.js
@@ -4,37 +4,46 @@ describe('AudioTool', () => {
 
     describe('getAudioInput', () => {
 
-      it('Should be async and return media stream', function (done) {
-        AudioTool.getAudioInput(audioCtx).then(function (result) {
-            expect(result.constructor.name).toBe('MediaStreamAudioSourceNode');
-            done();
+        it('Should be async and return media stream', function (done) {
+            AudioTool.getAudioInput(audioCtx).then(function (result) {
+                expect(result.constructor.name).toBe('MediaStreamAudioSourceNode');
+                done();
+            });
         });
-      });
 
     });
-    
-    describe('::getAnalyser', () => {
-      
-      it('returns an audio analyser node', function () {
-        let stream = jasmine.createSpyObj('AudioNode', {
-          'connect': true
-        });
-        let analyser = AudioTool.getAnalyser(stream, audioCtx);
 
-        expect(analyser.constructor.name).toEqual('AnalyserNode');
-      });
+    describe('::getAnalyser', () => {
+
+        it('returns an audio analyser node', function () {
+            let stream = jasmine.createSpyObj('AudioNode', {
+                'connect': true
+            });
+            let analyser = AudioTool.getAnalyser(stream, audioCtx);
+
+            expect(analyser.constructor.name).toEqual('AnalyserNode');
+        });
 
     });
 
     describe('::getLevels', () => {
-      it('returns an array', () => {
-        let stream = jasmine.createSpyObj('AudioNode', {
-          'connect': true
-        });
-        let analyser = AudioTool.getAnalyser(stream, audioCtx);
+        it('returns an array', () => {
+            let stream = jasmine.createSpyObj('AudioNode', {
+                'connect': true
+            });
+            let analyser = AudioTool.getAnalyser(stream, audioCtx);
 
-        expect(AudioTool.getLevels(analyser)).toEqual(jasmine.any(Uint8Array))
-      });
+            expect(AudioTool.getLevels(analyser)).toEqual(jasmine.any(Uint8Array))
+        });
+
+        it('it should be the correct length', () => {
+            let stream = jasmine.createSpyObj('AudioNode', {
+                'connect': true
+            });
+            let analyser = AudioTool.getAnalyser(stream, audioCtx);
+
+            expect(AudioTool.getLevels(analyser).length).toEqual(1024)
+        })
     });
-    
+
 });

ee077cea7203581a8fb6e0bce31b3a1d6fe3e5e3
RMaughan93 <rhys.maughan93@gmail.com>
2019-10-04 16:26:42 +0100

    refactored index.html, extracted all JS to their own files, refactored file structure

---
+++ src/Animations/shapesAnimation.js
@@ -0,0 +1,28 @@
+const sphere1 = new Sphere(150, -500, 0, 0, scene);
+const cube1 = new Cube(200, 200, 200, 0, 0, 0, scene);
+const tetra1 = new Tetra(200, 500, 0, 0, scene);
+
+let sphereScale = 1;
+let cubeScale = 1;
+let tetraScale = 1;
+
+function mainLoop(){
+  if (analyser) {
+    sphereScale = AudioTool.getBassScale(analyser);
+    cubeScale = AudioTool.getMidScale(analyser);
+    tetraScale = AudioTool.getTrebleScale(analyser);
+  }
+
+  sphere1.changeScale(sphereScale);
+  cube1.changeScale(cubeScale);
+  tetra1.changeScale(tetraScale);
+  sphere1.sphere.rotation.z += 0.01;
+  cube1.cube.rotation.x += 0.01;
+  cube1.cube.rotation.y += 0.01;
+  tetra1.tetra.rotation.x += 0.01;
+  tetra1.tetra.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(mainLoop);
+}
+mainLoop();

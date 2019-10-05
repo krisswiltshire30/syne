02f988ce34caecb8e4a159ec53defcfdb0a5c407
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-10-05 19:42:42 +0100

    Added dat.gui to project - Basic folders created

---
+++ src/guiRender.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+  var params = {
+    color: 0xff0000
+  };
+
+  var gui = new dat.GUI();
+  var cubeFolder = gui.addFolder('Cube');
+  var sphereFolder = gui.addFolder('Sphere');
+  var tetraFolder = gui.addFolder('Tetra');
+
+
+};

fe588985126fc46a851206fb422f30bb1e1e3881
Guillaume <liard.guillaume@gmail.com>
2019-10-07 11:42:56 +0100

    cleared up non audio related features

--- src/Animations/shapesAnimation.js
+++ src/Animations/shapesAnimation.js
@@ -1,44 +1,28 @@
-const sphere1 = new Sphere(150, -250, 0, 0, scene);
-const cube1 = new Cube(200, 200, 200, 250, 0, 0, scene);
-const tetra1 = new Tetra(200, 700, 0, 0, scene);
-const torus1 = new Torus(100, -700, 0, 0, scene);
+const sphere1 = new Sphere(150, -500, 0, 0, scene);
+const cube1 = new Cube(200, 200, 200, 0, 0, 0, scene);
+const tetra1 = new Tetra(200, 500, 0, 0, scene);
 
 let sphereScale = 1;
 let cubeScale = 1;
 let tetraScale = 1;
-let torusScale = 1;
-let sphereRotation = 0.01;
-let cubeRotation = 0.01;
-let tetraRotation = 0.01;
-let torusRotation = 0.01;
 
-function mainLoop() {
+function mainLoop(){
   if (analyser) {
     sphereScale = AudioTool.getBassScale(analyser);
     cubeScale = AudioTool.getMidScale(analyser);
     tetraScale = AudioTool.getTrebleScale(analyser);
-    torusScale = AudioTool.getSubBassScale(analyser);
-    sphereRotation = AudioToolGraphics.getBassDecimal(analyser);
-    cubeRotation = AudioToolGraphics.getMidDecimal(analyser);
-    tetraRotation = AudioToolGraphics.getTrebleDecimal(analyser);
-    torusRotation = AudioToolGraphics.getSubBassDecimal(analyser);
-
   }
 
   sphere1.changeScale(sphereScale);
   cube1.changeScale(cubeScale);
   tetra1.changeScale(tetraScale);
-  torus1.changeScale(torusScale);
-  sphere1.shape.rotation.z += sphereRotation;
-  cube1.shape.rotation.x += cubeRotation;
-  cube1.shape.rotation.y += cubeRotation;
-  tetra1.shape.rotation.x += tetraRotation;
-  tetra1.shape.rotation.y += tetraRotation;
-  torus1.shape.rotation.x += torusRotation;
-  torus1.shape.rotation.y += torusRotation;
+  sphere1.shape.rotation.z += 0.01;
+  cube1.shape.rotation.x += 0.01;
+  cube1.shape.rotation.y += 0.01;
+  tetra1.shape.rotation.x += 0.01;
+  tetra1.shape.rotation.y += 0.01;
 
   renderer.render(scene, camera);
   requestAnimationFrame(mainLoop);
 }
-
 mainLoop();

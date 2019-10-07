0f2c5077c312413f8afce21bd51f96b2c9bef13e
RMaughan93 <rhys.maughan93@gmail.com>
2019-10-07 18:48:46 +0100

    refactored energies into new classes

--- src/AudioTool/Energies/sub.js
+++ src/AudioTool/Energies/sub.js
@@ -1,13 +1,3 @@
-const Sub = {
-    getEnergy: () => { return AudioTool.getLevels().slice(0,3); },
-    getAvg: function (percent) { 
-        value = this.sum(this.getEnergy()) / this.getEnergy().length 
-        if (percent) {
-            value =  AudioTool.getPercent(value)
-        }
-        return value
-    },
-    sum: (array) => { return array.reduce((a, b) => a + b, 0 ) },
-    getMaxLevel: function () { return Math.max(...this.getEnergy()) },
-    getScale: function() { return this.getAvg(true) + 1}
-}
+const Sub = new Energy();
+    
+Sub.getEnergy = () => { return AudioTool.getLevels().slice(0,3); }

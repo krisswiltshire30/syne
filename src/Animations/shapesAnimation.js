d6159c0baf9ccd9c49dec6759db7be9cd0782050
fentonhaslam <fentonhaslam@gmail.com>
2019-10-07 16:17:07 +0100

    initial changes to orbit in solid class

--- src/Animations/shapesAnimation.js
+++ src/Animations/shapesAnimation.js
@@ -3,27 +3,33 @@ AudioTool.setup();
 const sphere1 = new Sphere(150, -500, 0, 0, scene);
 const cube1 = new Cube(200, 200, 200, 0, 0, 0, scene);
 const tetra1 = new Tetra(200, 500, 0, 0, scene);
+const sphere2 = new Sphere(100, -1000, 0, 0, scene);
 
 let sphereScale = 1;
 let cubeScale = 1;
 let tetraScale = 1;
+let sphere2Scale = 1;
+
 
 function mainLoop() {
   if (AudioTool.isSetup) {
     sphereScale = 1 + AudioTool.getBassAverage(true);
     cubeScale = 1 + AudioTool.getMidAverage(true);
     tetraScale = 1 + AudioTool.getTrebleAverage(true);
-  }
+    sphere2Scale = 1 + AudioTool.getBassAverage(true);
 
+  }
+  sphere2.changeScale(sphere2Scale);
   sphere1.changeScale(sphereScale);
   cube1.changeScale(cubeScale);
   tetra1.changeScale(tetraScale);
+  sphere2.shape.rotation.x += 0.01;
   sphere1.shape.rotation.z += 0.01;
   cube1.shape.rotation.x += 0.01;
   cube1.shape.rotation.y += 0.01;
   tetra1.shape.rotation.x += 0.01;
   tetra1.shape.rotation.y += 0.01;
-
+  sphere2.orbit(sphere1);
   renderer.render(scene, camera);
   requestAnimationFrame(mainLoop);
 }
--- src/Shapes/solid.js
+++ src/Shapes/solid.js
@@ -1,9 +1,12 @@
 class Solid {
-  constructor(scene) {
+  constructor(scene, orbitAlt, orbitAngle, orbitAngleMod) {
     this.scene = scene;
     this.material = new THREE.MeshNormalMaterial({
       wireframe: true
     });
+    this.orbitAlt = orbitAlt;
+    this.orbitAngle = orbitAngle;
+    this.orbitAngleMod = orbitAngleMod;
   }
 
   changeOpacity(sizeScaler) {
@@ -28,5 +31,14 @@ class Solid {
     this.shape.position.y = posY;
     this.shape.position.z = posZ;
     return this.shape.position
+  }
+
+  orbit(object) {
+    this.shape.position.x = object.shape.position.x + this.orbitAlt * Math.cos((this.orbitAngle));
+    this.shape.position.y = object.shape.position.y + this.orbitAlt * Math.sin((this.orbitAngle));
+    this.orbitAngle = this.orbitAngle + this.orbitAngleMod;
+    if (this.orbitAngle >= 360) {
+      this.orbitAngle = 0;
+    }
   }
 }

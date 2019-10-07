58620f42b10cfcdb5ad893f9cc77a4ee3593b1ea
RMaughan93 <rhys.maughan93@gmail.com>
2019-10-07 12:04:11 +0100

    fixing unresolved merge issues

--- src/Animations/shapesAnimation.js
+++ src/Animations/shapesAnimation.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 const sphere1 = new Sphere(150, -250, 0, 0, scene);
 const cube1 = new Cube(200, 200, 200, 250, 0, 0, scene);
 const tetra1 = new Tetra(200, 750, 0, 0, scene);
 const torus1 = new Torus(100, -750, 0, 0, scene);
-=======
-const sphere1 = new Sphere(150, -500, 0, 0, scene);
-const cube1 = new Cube(200, 200, 200, 0, 0, 0, scene);
-const tetra1 = new Tetra(200, 500, 0, 0, scene);
->>>>>>> fe588985126fc46a851206fb422f30bb1e1e3881
 
 let sphereScale = 1;
 let cubeScale = 1;
 let tetraScale = 1;
-<<<<<<< HEAD
 let torusScale = 1;
 let timerThing = 0;
 
@@ -22,14 +15,6 @@ function mainLoop(){
     cubeScale = AudioToolGraphics.getMidScale(analyser);
     tetraScale = AudioToolGraphics.getTrebleScale(analyser);
     torusScale = AudioToolGraphics.getSubBassScale(analyser);
-=======
-
-function mainLoop(){
-  if (analyser) {
-    sphereScale = AudioTool.getBassScale(analyser);
-    cubeScale = AudioTool.getMidScale(analyser);
-    tetraScale = AudioTool.getTrebleScale(analyser);
->>>>>>> fe588985126fc46a851206fb422f30bb1e1e3881
   }
 
   sphere1.changeScale(sphereScale);
@@ -40,7 +25,6 @@ function mainLoop(){
   cube1.shape.rotation.y += 0.01;
   tetra1.shape.rotation.x += 0.01;
   tetra1.shape.rotation.y += 0.01;
-<<<<<<< HEAD
   torus1.shape.rotation.x += 0.01;
   torus1.shape.rotation.y += 0.01;
   if (timerThing < 1500 ) {
@@ -70,10 +54,9 @@ function mainLoop(){
   } else {
     timerThing = 0;
   }
-=======
->>>>>>> fe588985126fc46a851206fb422f30bb1e1e3881
 
   renderer.render(scene, camera);
   requestAnimationFrame(mainLoop);
 }
 mainLoop();
+

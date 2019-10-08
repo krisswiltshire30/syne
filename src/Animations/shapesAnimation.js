db58f5e7ebeb146a98f8c046d06792922b633e4d
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-10-08 23:48:44 +0100

    Minor spacing fix

--- src/Animations/shapesAnimation.js
+++ src/Animations/shapesAnimation.js
@@ -1,25 +1,5 @@
 AudioTool.setup();
 
-<<<<<<< HEAD
-=======
-const centreSphere = new Sphere(70, 0, 0, 0, scene);
-//const cube1 = new Cube(200, 200, 200, 0, 0, 0, scene);
-//const tetra1 = new Tetra(200, 500, 0, 0, scene);
-//const sphere2 = new Sphere(100, -1000, 0, 0, scene, 600);
-//const sphere3 = new Sphere(20, -750, 0, 0, scene, 3);
-const sphereOrbit1 = new Poly(60, 1000, 0, 0, scene, 500);
-const sphereOrbit2 = new Sphere(50, 1200, 0, 0, scene, 850);
-
-sphereOrbit1.orbitAngleMod = 1
-sphereOrbit2.orbitAngleMod = 2
-
-sphereScale = 1;
-//cubeScale = 1;
-//tetraScale = 1;
-sphereOrbit1Scale = 1;
-sphereOrbit2Scale = 1;
-
->>>>>>> 7e2922c91bc3a1b77fbcd0d3f1ca14ae8b140ca5
 var animationToggles = {
   sphereRotate: true,
   sphereRotateSpeed: 0.01,
@@ -31,7 +11,6 @@ var animationToggles = {
   preset: 'A'
 }
 
-<<<<<<< HEAD
 function shapeCreator() {
   if (presetOne) {
     this.sphere1 = new Sphere(200, -500, 0, 0, scene);
@@ -45,19 +24,9 @@ function shapeCreator() {
     newPlanet.orbitAngleMod = 2
   } else {
     return
-=======
-function mainLoop() {
-  if (AudioTool.isSetup) {
-    sphereScale = Bass.getScale(true);
-    //cubeScale = Mids.getScale(true);
-    //tetraScale = Treble.getScale(true);
-    sphereOrbit1Scale = Treble.getScale(true);
-    sphereOrbit2Scale = Mids.getScale(true);
->>>>>>> 7e2922c91bc3a1b77fbcd0d3f1ca14ae8b140ca5
   }
 }
 
-<<<<<<< HEAD
 sphereScale = 1;
 cubeScale = 1;
 tetraScale = 1;
@@ -104,26 +73,6 @@ function mainLoop() {
     }
     orbitAnimation();
   }
-=======
-  sphereOrbit1.changeScale(sphereOrbit1Scale);
-  //sphere2.changeScale(sphere2Scale);
-  centreSphere.changeScale(sphereScale);
-  //cube1.changeScale(cubeScale);
-  //tetra1.changeScale(tetraScale);
-  sphereOrbit1.orbit(centreSphere);
-  sphereOrbit2.orbit(centreSphere);
-
-  if (animationToggles.sphereRotate) {
-    centreSphere.changeRotation(animationToggles.sphereRotateSpeed, 0, 0);
-  }
-  //if (animationToggles.cubeRotate) {
-  //  cube1.changeRotation(animationToggles.cubeRotateSpeed, animationToggles.cubeRotateSpeed, 0);
-  //}
-  //if (animationToggles.tetraRotate) {
-  //  tetra1.changeRotation(animationToggles.tetraRotateSpeed, animationToggles.tetraRotateSpeed, 0);
-  //}
-  sphereOrbit1.shape.rotation.y += 0.01
->>>>>>> 7e2922c91bc3a1b77fbcd0d3f1ca14ae8b140ca5
 
   cameraUpdate();
 
--- src/Shapes/poly.js
+++ src/Shapes/poly.js
@@ -2,7 +2,6 @@ class Poly extends Solid {
     constructor(radius, posX, posY, posZ, scene, orbitAlt, orbitAngle = 0, orbitAngleMod = 1) {
         super(scene, orbitAlt, orbitAngle, orbitAngleMod);
         var points = [];
-<<<<<<< HEAD
         this.visible;
         for (var i = 0; i < 81; i++) {
             points.push(new THREE.Vector2(Math.sin(i * 0.1) * 50 + 200, (i - 1) * 0.1));
@@ -13,16 +12,6 @@ class Poly extends Solid {
             color: "#003e98",
             visible: this.visible,
 
-=======
-        this.wireframe = true;
-        for (var i = 0; i < 80; i++) {
-            points.push(new THREE.Vector2(Math.sin(i * 100) * 40 + 200, (i - 1) * 10));
-        }
-        var box = new THREE.LatheGeometry(points);
-        var smallMaterial = new THREE.MeshNormalMaterial({
-            color: new THREE.Color('green'),
-            wireframe: this.wireframe
->>>>>>> 7e2922c91bc3a1b77fbcd0d3f1ca14ae8b140ca5
         });
         var boxMesh = new THREE.Mesh(box, smallMaterial);
         var avocado = new THREE.SphereGeometry(radius, 64, 64);

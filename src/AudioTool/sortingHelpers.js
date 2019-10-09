7a4fd3a54d82fefc24150072b29c74d150b846a3
GuyCalloway <[guyfcalloway@gmail.com]>
2019-10-09 10:28:07 +0100

    Sorted Linter

--- src/AudioTool/sortingHelpers.js
+++ src/AudioTool/sortingHelpers.js
@@ -1,48 +1,48 @@
 const SortingHelpers = {
 
-    splitEnergyArrays: function (usArray, quartile, sorted) {
-
-        if (sorted) {
-            var array = this.sortNumberArray(usArray);
-        } else {
-            var array = usArray;
-        }
-
-        var x = (array.length / 4);
-
-        return array.slice(((quartile - 1) * x), ((quartile) * x))
-
-    },
-
-    getLocalMaxima: function (array) {
-        var a = array;
-        maxes = []
-        for (var i = 1; i < a.length - 1; ++i) {
-            if (a[i - 1] < a[i] && a[i] > a[i + 1])
-                maxes.push({
-                    strength: a[i],
-                    index: i
-                })
-        }
-        return this.sortArrayOfObjectsArray(maxes, strength);
-    },
-
-    sortArrayOfObjects: function (array, key) {
-        array.sort(function (a, b) {
-            return a[key] < b[key]
-        });
-        return array;
-    },
-
-    sortNumberArray: function (array) {
-        array.sort(function (a, b) {
-            return a - b;
-        })
-        return array;
-    },
-
-    getIndexOfMax: function (arr) {
-        let i = arr.indexOf(Math.max(...arr));
-        return i;
+  splitEnergyArrays: function (usArray, quartile, sorted) {
+    var array1 = []
+    if (sorted) {
+      array1 = this.sortNumberArray(usArray);
+    } else {
+      array1 = usArray;
     }
+
+    var x = (array1.length / 4);
+
+    return array1.slice(((quartile - 1) * x), ((quartile) * x))
+
+  },
+
+  getLocalMaxima: function (array) {
+    var a = array;
+    maxes = []
+    for (var i = 1; i < a.length - 1; ++i) {
+      if (a[i - 1] < a[i] && a[i] > a[i + 1])
+        maxes.push({
+          strength: a[i],
+          index: i
+        })
+    }
+    return this.sortArrayOfObjectsArray(maxes, strength);
+  },
+
+  sortArrayOfObjects: function (obArray, key) {
+    obAarray.sort(function (a, b) {
+      return a[key] < b[key]
+    });
+    return obArray;
+  },
+
+  sortNumberArray: function (array) {
+    array.sort(function (a, b) {
+      return a - b;
+    })
+    return array;
+  },
+
+  getIndexOfMax: function (arr) {
+    let i = arr.indexOf(Math.max(...arr));
+    return i;
+  }
 };

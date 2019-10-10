283d90fcf88b52703c218f8534758e0ab6a811b4
GuyCalloway <[guyfcalloway@gmail.com]>
2019-10-10 15:04:34 +0100

    Math Helpers tests

--- spec/AudioTool/mathHelpersSpec.js
+++ spec/AudioTool/mathHelpersSpec.js
@@ -1,23 +1,25 @@
 describe('mathHelpers', () => {
 
     describe('standardDeviationVariance', () => {
-        it('should return variance when called with true', () => {
+        it('should return standard deviation when called with true', () => {
             let stdDev = MathHelpers.standardDeviationVariance([1, 1, 1, 1], false)
             expect(stdDev).toEqual(0);
         });
-        // it('should return an array of the correct length', () => {
-        //     expect(Bass.getEnergy().length).toEqual(22);
-        // });
-        // it('should contain integer values', () => {
-        //     expect(Bass.getEnergy()[0]).toEqual(jasmine.any(Number));
+        it('should return st when called with true', () => {
+            let variance = MathHelpers.standardDeviationVariance([1, 2, 2, 2, 2, 2, 3, 4, 6, 10], true)
+            expect(variance.toFixed(2)).toEqual('6.64');
+        });
+    });
+
+    describe('linear regression', () => {
+        it('should return linear Regression', () => {
+            let linR = MathHelpers.linearRegression([5, 2, 1, 4], true)
+            expect(linR.toFixed(2)).toEqual('0.98');
+        });
+
+        it('should return linear Regression', () => {
+            let linR = MathHelpers.linearRegression([1, 2, 4, 5], false)
+            expect(linR.toFixed(2)).toEqual('0.98');
+        });
     });
 });
-
-// describe(':getAvg', () => {
-//     it('should return a numeral', () => {
-//         expect(Bass.getAvg()).toEqual(jasmine.any(Number));
-//     });
-//     it('should return a numeral < 1 when passed true as an argument', () => {
-//         expect(Bass.getAvg(true)).toBeLessThan(1);
-//     });
-// });
